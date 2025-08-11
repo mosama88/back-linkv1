4edb80985959ad1a00dfe138053799e407e21bd9
mosama88 <mosama88@hotmail>
2025-08-11 21:49:32 +0300

    balances

---
+++ resources/views/dashboard/layouts/head.blade.php
@@ -0,0 +1,24 @@
+<meta charset="utf-8" />
+    <title>Back Link | @yield('title')</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Premium Bootstrap 5 Landing Page Template" />
+    <meta name="keywords" content="Saas, Software, multi-uses, HTML, Clean, Modern" />
+    <meta name="author" content="Shreethemes" />
+    <meta name="email" content="support@shreethemes.in" />
+    <meta name="website" content="https://shreethemes.in" />
+    <meta name="Version" content="v4.8.0" />
+
+    <!-- favicon -->
+    <link rel="shortcut icon" href="{{asset('dashboard')}}/assets/images/favicon.ico" />
+    <!-- Css -->
+    <link href="{{asset('dashboard')}}/assets/libs/simplebar/simplebar.min.css" rel="stylesheet">
+    <!-- Bootstrap Css -->
+    <link href="{{asset('dashboard')}}/assets/css/bootstrap-rtl.min.css" class="theme-opt" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="{{asset('dashboard')}}/assets/libs/@mdi/font/css/materialdesignicons.min.css" rel="stylesheet" type="text/css">
+    <link href="{{asset('dashboard')}}/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{asset('dashboard')}}/assets/libs/@iconscout/unicons/css/line.css" type="text/css" rel="stylesheet" />
+    <!-- Style Css-->
+    <link href="{{asset('dashboard')}}/assets/css/style-rtl.min.css" class="theme-opt" rel="stylesheet" type="text/css" />
+
+    @stack('css')
--- routes/admin.php
+++ routes/admin.php
@@ -1,12 +1,21 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Dashboard\Balanceontroller;
+use App\Http\Controllers\Dashboard\BackLinkController;
 
 
 Route::get('/', function () {
     return view('dashboard.index');
 })->middleware(['auth', 'verified'])->name('index');
+
+
+Route::middleware('auth:admin')->group(function () {
+    Route::resource('balances', Balanceontroller::class);
+    Route::resource('back-links', BackLinkController::class);
+});
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
