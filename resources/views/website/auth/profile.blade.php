7217e890618235ffc437cbd63a9388133e4c10fb
mosama88 <mosama88@hotmail>
2025-08-31 13:27:54 +0300

    Done Good2

--- resources/views/website/auth/profile.blade.php
+++ resources/views/website/auth/profile.blade.php
@@ -16,16 +16,7 @@
                 </div> <!--end col-->
             </div><!--end row-->
 
-            <div class="position-breadcrumb">
-                <nav aria-label="breadcrumb" class="d-inline-block">
-                    <ul class="breadcrumb rounded shadow mb-0 px-4 py-2">
-                        <li class="breadcrumb-item"><a href="index.html">Landrick</a></li>
-                        <li class="breadcrumb-item"><a href="#">Page</a></li>
-                        <li class="breadcrumb-item"><a href="#">Contact</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Contact Three</li>
-                    </ul>
-                </nav>
-            </div>
+           @include('website.layouts.breadcrumb')
         </div> <!--end container-->
     </section><!--end section-->
     <!-- Hero End -->
---
+++ resources/views/website/layouts/breadcrumb.blade.php
@@ -0,0 +1,10 @@
+ <div class="position-breadcrumb">
+     <nav aria-label="breadcrumb" class="d-inline-block">
+         <ul class="breadcrumb rounded shadow mb-0 px-4 py-2">
+             <li class="breadcrumb-item"><a href="index.html">Landrick</a></li>
+             <li class="breadcrumb-item"><a href="#">Page</a></li>
+             <li class="breadcrumb-item"><a href="#">Contact</a></li>
+             <li class="breadcrumb-item active" aria-current="page">Contact Three</li>
+         </ul>
+     </nav>
+ </div>
